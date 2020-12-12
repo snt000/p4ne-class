364753375b82749a376e940d18c140e3a8a8cfc9
Stephen Groombridge <steve@snt.co.uk>
2020-12-12 12:53:20 +0000

    First commit

---
+++ 04 Python and telnet/shver.py
@@ -0,0 +1,20 @@
+import getpass
+import telnetlib
+
+HOST = "10.99.99.10"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"terminal length 0\n")
+tn.write(b"sh ver\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
